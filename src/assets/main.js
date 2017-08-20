67483dd30ee7dfc1b7a2b7799bb95a70ff9bfbe1
sk-scd91 <scdeneen@gmail.com>
2017-08-20 15:37:35 -0400

    Completed fetching badges from javascript.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,26 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/1256656.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      var badges = $('#badges');
+      // handle response
+      response.courses.completed.forEach(function(course) {
+        var courseElem = $('<div></div>').addClass('course');
+
+        courseElem.append($('<h3>' + course.title + '</h3>'));
+        courseElem.append($('<img></img>').attr('src', course.badge));
+        courseElem.append($('<a>See Course</a>')
+          .attr({
+            target: '_blank',
+            href: course.url})
+          .addClass('btn btn-primary'));
+
+        badges.append(courseElem);
+      }, this);
+    }
+  });
 
 });
